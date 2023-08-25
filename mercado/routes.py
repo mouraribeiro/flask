4fd81fc85c8843c2104e4cbe6211e32234c2c137
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2023-08-25 09:56:18 -0300

    validators

--- mercado/routes.py
+++ mercado/routes.py
@@ -1,7 +1,8 @@
 from mercado import app
-from flask import render_template
-from mercado.models import Item
+from flask import render_template,redirect,url_for
+from mercado.models import Item,User
 from mercado.forms import CadastroForm
+from mercado import db
 
 @app.route('/')
 def page_home():
@@ -13,7 +14,17 @@ def page_produtos():
     itens = Item.query.all()
     return render_template("products.html", itens=itens)
 
-@app.route('/cadastro')
+@app.route('/cadastro',methods=['GET','POST'])
 def page_cadastro():
     form= CadastroForm()
+    if form.validate_on_submit():
+        usuario = User(
+            usuario = form.usuario.data,
+            email = form.email.data,
+            senha = form.senha1.data
+        )
+        
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('page_produtos'))
     return render_template('cadastro.html', form=form)
